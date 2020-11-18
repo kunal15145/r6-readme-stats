05684a455b1242a7d7a016b883ee17453de0719f
kunal15145 <kunal15145@iiitd.ac.in>
2020-11-18 19:06:07 +0530

    Get r6 stats

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
+var urllib = require('urllib');
+
+function userData(username) {
+  return new Promise((resolve, reject) => {
+    urllib.request(`https://r6stats.com/api/player-search/${username}/pc`).then(function (result) {
+      resolve(result)
+    }).catch(function (err) {
+      reject(err)
+    });
+  });
+}
+
 module.exports = (req, res) => {
     const {
       username
-    } = req.query
-    return res.send(username)
-  }  
+    } = req.query;
+    userData(username).then(ret => {
+      return res.send(JSON.parse(ret.data.toString()))
+    })
+  }
